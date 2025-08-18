77c0eb083e9f6c76fc80a514c1b3eab9cce49c8b
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-08-18 22:46:51 +0300

    Delete states directory

--- states/registration.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class Reg(StatesGroup):
-    name = State()
-    age = State()
-    gender = State()
-    location = State()
-    activities = State()
